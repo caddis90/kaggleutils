6bc662defaf4decad177dbb168323c9a4f7e074e
Carole Addis <caroleaddis@Caroles-MacBook-Air.local>
2023-04-06 21:29:05 +0100

    Minor

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import logging
 
 def get_training_data():
-    logging.info("Loading training data...")
+    print("Loading training data...")
     return pd.read_csv("/kaggle/input/facial-keypoints-detection/training.zip")
